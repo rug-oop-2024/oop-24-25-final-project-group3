1fc5ba8ab9939ae064bc17193a423b1be1927c6a
atodorov284 <atodorov284@gmail.com>
2024-10-28 19:28:41 +0100

    update

--- autoop/functional/feature.py
+++ autoop/functional/feature.py
@@ -1,9 +1,9 @@
+
 from typing import List
 from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.feature import Feature
 import pandas as pd
 from pandas.api.types import CategoricalDtype
-
 
 def detect_feature_types(dataset: Dataset) -> List[Feature]:
     """Detects feature types (categorical or numerical) in a dataset.
@@ -14,11 +14,4 @@ def detect_feature_types(dataset: Dataset) -> List[Feature]:
     Returns:
         List[Feature]: A list of features with detected types.
     """
-    ds = dataset.read()
-    features = []
-    for name in ds.columns:
-        if ds[name].dtype == "object":
-            features.append(Feature(name=name, type="categorical"))
-        else:
-            features.append(Feature(name=name, type="numerical"))
-    return features
+    raise NotImplementedError("This should be implemented by you.")
