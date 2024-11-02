4660d0ef25f81948568ee4e04da2261e2b68f965
karbolak <kajetan.karbowski@gmail.com>
2024-11-02 16:53:59 +0100

    Part II: Streamlit functionalities implemented up to the dataset split and training within Modelling

--- autoop/core/ml/model/__init__.py
+++ autoop/core/ml/model/__init__.py
@@ -1,15 +1,13 @@
-
 from autoop.core.ml.model.model import Model
-from autoop.core.ml.model.regression import MultipleLinearRegression
-from autoop.core.ml.model.regression import RidgeRegression
-from autoop.core.ml.model.regression import DecisionTreeRegression
-from autoop.core.ml.model.classification import LogisticRegression
-from autoop.core.ml.model.classification import KNearestNeighbors
-from autoop.core.ml.model.classification import DecisionTreeClassification
+from autoop.core.ml.model.regression import (MultipleLinearRegression,
+                                             RidgeRegression,
+                                             DecisionTreeRegression)
+from autoop.core.ml.model.classification import (LogisticRegression,
+                                                 KNearestNeighbors,
+                                                 DecisionTreeClassification)
 
 REGRESSION_MODELS = ["MultipleLinearRegression", "RidgeRegression",
                      "DecisionTreeRegression"]
-
 CLASSIFICATION_MODELS = ["LogisticRegression", "KNearestNeighbors",
                          "DecisionTreeClassification"]
 
@@ -31,17 +29,17 @@ def get_model(model_name: str) -> Model:
     model_mapping = {
         "MultipleLinearRegression": MultipleLinearRegression,
         "RidgeRegression": RidgeRegression,
-        "DecisionTreeRegressorModel": DecisionTreeRegression,
+        "DecisionTreeRegression": DecisionTreeRegression,
         "LogisticRegression": LogisticRegression,
         "KNearestNeighbors": KNearestNeighbors,
-        "DecisionTreeClassifierModel": DecisionTreeClassification,
+        "DecisionTreeClassification": DecisionTreeClassification,
     }
 
     # Check if the model_name exists in the mapping
     if model_name not in model_mapping:
-        raise ValueError(f"Model '{model_name}' is not implemented. Available"
-                         f" models are: {', '.join(REGRESSION_MODELS +
-                                         CLASSIFICATION_MODELS)}")
+        raise ValueError(f"Model '{model_name}' is not implemented. Available "
+                         f"models are: {', '.join(REGRESSION_MODELS +
+                                        CLASSIFICATION_MODELS)}")
 
     # Return an instance of the requested model
     return model_mapping[model_name]()
